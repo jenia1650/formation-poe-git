2ee7c93e22a2c4e70496eeaa884a57777fc82d8e
Evgeniia Mochkaeva <evgeniiamochkaeva@gmail.com>
2022-01-17 14:10:04 +0100

    ajouter

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-function additionner(nombre1, nombre2) {
+function somme(nombre1, nombre2) {
 
     return nombre1 + nombre2;
     
@@ -6,7 +6,7 @@ function additionner(nombre1, nombre2) {
     
     
     
-    function diviser(nombre1, nombre2) {
+    function division(nombre1, nombre2) {
     
     return nombre1 / nombre2;
     
