9d5b45a838da1904fe1836323d586d2b09d1bb09
Evgeniia Mochkaeva <evgeniiamochkaeva@gmail.com>
2022-01-17 11:46:43 +0100

    Ajogit commit -majout

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function additionner(nombre1, nombre2) {
+
+    return nombre1 + nombre2;
+    
+    }
+    
+    
+    
+    function diviser(nombre1, nombre2) {
+    
+    return nombre1 / nombre2;
+    
+    }
